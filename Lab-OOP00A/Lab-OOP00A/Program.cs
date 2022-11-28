f7cfea53aeee0a55a4e0cc0501d5965395eab44d
MahakkapanPlangklang <119285823+MahakkapanPlangklang@users.noreply.github.com>
2022-11-28 15:40:14 +0700

    Add

---
+++ Lab-OOP00A/Lab-OOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Name
+Console.WriteLine("Mahakkapan Plangklang");
+//ID
+Console.WriteLine("653450296-6");
+//E-mail
+Console.WriteLine("mahakkapan.p@kkumail.com");
